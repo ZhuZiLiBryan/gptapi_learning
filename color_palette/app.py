a6c9352c5f5b93c7f860f7e31a5c9af835e3c551
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-01 14:14:17 -0700

    interfaced flask with openai API

--- color_palette/app.py
+++ color_palette/app.py
@@ -1,4 +1,9 @@
+import openai
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
+
+config = dotenv_values('../.env')
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__,
     template_folder='templates'
@@ -7,10 +12,20 @@ app = Flask(__name__,
 # Index route
 @app.route("/")
 def index():
-    return "First Flask Program!"
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role":"user", "content": "Give me the name of a U.S. president: "
+            }
+        ]
+    )
 
-@app.route("/test")
-def test():
+    return response.choices[0].message.content
+    #return render_template("index.html")
+
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
     return "Testing flask routes"
 
 if __name__=="__main__":
